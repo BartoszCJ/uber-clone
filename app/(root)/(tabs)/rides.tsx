63e3003bb90d85ff34d7c44a9069325370f573be
BartoszCoJest <bartoszcojest@gmail.com>
2024-11-14 18:46:08 +0100

    Added folders

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>Homeexd</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
